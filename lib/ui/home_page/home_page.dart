c8399cc06970989128037f390dadeda4c0914fa3
BlueSky0322 <ryanng8628@gmail.com>
2023-06-25 01:24:30 +0800

    feat: basic UI

--- lib/ui/home_page/home_page.dart
+++ lib/ui/home_page/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_eye_fyp/ui/home_page/home_page_state.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 import '../../utils/constants.dart';
 
@@ -7,17 +10,106 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Color.fromARGB(255, 142, 197, 241),
-      elevation: 0,
-      toolbarHeight: 70,
-      title: const Align(
-        alignment: Alignment.centerLeft,
-        child: Text(
-          "Welcome Back!",
-          style: TextStyle(
-              fontFamily: 'Outfit', fontSize: 20, color: Colors.black),
+    final state = Provider.of<HomePageState>(context);
+    return Scaffold(
+      backgroundColor: Colors.lightBlue.shade100,
+      appBar: AppBar(
+        backgroundColor: primaryBG,
+        elevation: 0,
+        toolbarHeight: 70,
+        title: const Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            "Welcome Back",
+            style: TextStyle(
+              fontFamily: 'Outfit',
+              fontSize: 28,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+            ),
+          ),
         ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 5,
+                    blurRadius: 20,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(12.0),
+                ),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 0, 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "You have ${state.expItemCount} items \nthat are still fresh!",
+                          textAlign: TextAlign.start,
+                          style: const TextStyle(
+                            fontFamily: 'Outfit',
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        Text.rich(
+                          TextSpan(
+                              text: "Enjoy by   ",
+                              style: const TextStyle(
+                                fontFamily: 'Outfit',
+                                fontSize: 15,
+                                color: Colors.grey,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: DateFormat('dd/MM/yyyy')
+                                      .format(state.nearestExpDate),
+                                  style: const TextStyle(
+                                    fontFamily: 'Outfit',
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 29, 214, 106),
+                                  ),
+                                )
+                              ]),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Text(
+                    "Text",
+                    style: TextStyle(
+                      fontFamily: 'Outfit',
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/ui/home_page/home_page_state.dart
+++ lib/ui/home_page/home_page_state.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class HomePageState extends ChangeNotifier {
   final BuildContext context;
+  DateTime nearestExpDate = DateTime.now();
+  int expItemCount = 0;
 
   HomePageState(this.context);
 }
