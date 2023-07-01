32df3eb513fb1851719e1a770712d2aad605af0c
BlueSky0322 <ryanng8628@gmail.com>
2023-07-02 02:07:54 +0800

    feat: new nav bar UI

--- lib/ui/common/nav_bar.dart
+++ lib/ui/common/nav_bar.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:food_eye_fyp/utils/constants.dart';
 import 'package:provider/provider.dart';
@@ -10,29 +11,56 @@ class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final state = Provider.of<NavBarState>(context);
-    return BottomNavigationBar(
-      backgroundColor: appbar,
-      elevation: 3,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.apps_rounded),
-          label: 'Items',
+    return CurvedNavigationBar(
+      backgroundColor: pageBG,
+      buttonBackgroundColor: const Color.fromARGB(255, 49, 61, 114),
+      height: 55,
+      color: appbar,
+      animationDuration: const Duration(milliseconds: 300),
+      items: const [
+        Icon(
+          Icons.home,
+          size: 30,
+          color: appbarText,
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.my_library_add_outlined),
-          label: 'Add item',
+        Icon(
+          Icons.my_library_add_outlined,
+          size: 30,
+          color: appbarText,
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.trending_up_rounded),
-          label: 'Overview',
+        Icon(
+          Icons.trending_up_rounded,
+          size: 30,
+          color: appbarText,
         ),
       ],
-      type: BottomNavigationBarType.fixed,
-      currentIndex: state.selectedIndex,
-      selectedItemColor: Colors.teal,
-      unselectedItemColor: primaryBG,
-      iconSize: 25,
-      onTap: (index) => state.selectedIndex = index,
+      onTap: (index) {
+        state.selectedIndex = index;
+      },
     );
+    // return BottomNavigationBar(
+    //   backgroundColor: appbar,
+    //   elevation: 3,
+    //   items: const <BottomNavigationBarItem>[
+    //     BottomNavigationBarItem(
+    //       icon: Icon(Icons.apps_rounded),
+    //       label: 'Items',
+    //     ),
+    //     BottomNavigationBarItem(
+    //       icon: Icon(Icons.my_library_add_outlined),
+    //       label: 'Add item',
+    //     ),
+    //     BottomNavigationBarItem(
+    //       icon: Icon(Icons.trending_up_rounded),
+    //       label: 'Overview',
+    //     ),
+    //   ],
+    //   type: BottomNavigationBarType.fixed,
+    //   currentIndex: state.selectedIndex,
+    //   selectedItemColor: Colors.teal,
+    //   unselectedItemColor: primaryBG,
+    //   iconSize: 25,
+    //   onTap: (index) => state.selectedIndex = index,
+    // );
   }
 }
