326cc045d45dbb9e4f2354cf0ae6a180c15162f9
BlueSky0322 <ryanng8628@gmail.com>
2023-07-01 23:51:30 +0800

    feat: test api endpoint availibility

--- lib/ui/analytics_page/analytics_page.dart
+++ lib/ui/analytics_page/analytics_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:food_eye_fyp/ui/analytics_page/analytics_page_state.dart';
+import 'package:provider/provider.dart';
 
 class AnalyticsPage extends StatelessWidget {
   const AnalyticsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final state = Provider.of<AnalyticsPageState>(context);
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: () {
+              state.fetchWeatherForecast();
+            },
+            child: Text("Button"),
+          ),
+        ],
+      ),
+    );
   }
 }
