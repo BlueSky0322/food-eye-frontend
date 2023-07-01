6289cfe44a95afe5d239955b2fd454df1fb5efa1
BlueSky0322 <ryanng8628@gmail.com>
2023-07-01 23:49:19 +0800

    feat: toJson() and fromJson() convert functions

--- lib/data/model/item.dart
+++ lib/data/model/item.dart
@@ -18,8 +18,30 @@ class Item {
     required this.storedAt,
     required this.description,
   });
-}
 
-// final itemList = <Item>[
-  
-// ];
+  Map<String, dynamic> toJson() {
+    return {
+      'itemName': itemName,
+      'itemType': itemType,
+      'quantity': quantity,
+      'datePurchased': datePurchased?.toIso8601String(),
+      'dateExpiresOn': dateExpiresOn?.toIso8601String(),
+      'imagePath': imagePath,
+      'storedAt': storedAt,
+      'description': description,
+    };
+  }
+
+  factory Item.fromJson(Map<String, dynamic> json) {
+    return Item(
+      itemName: json['itemName'],
+      itemType: json['itemType'],
+      quantity: json['quantity'],
+      datePurchased: DateTime.parse(json['datePurchased']),
+      dateExpiresOn: DateTime.parse(json['dateExpiresOn']),
+      imagePath: json['imagePath'],
+      storedAt: json['storedAt'],
+      description: json['description'],
+    );
+  }
+}
