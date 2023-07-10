dda7655ee405150598c4f4ca690a054a8c28b888
BlueSky0322 <ryanng8628@gmail.com>
2023-07-10 14:17:38 +0800

    feat: ensure initialized for Notification Service

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:food_eye_fyp/service/notification_service.dart';
 import 'package:food_eye_fyp/ui/common/nav_bar_state.dart';
 import 'package:food_eye_fyp/ui/common/nav_bar_wrapper.dart';
 import 'package:food_eye_fyp/ui/splash_page/splash_page.dart';
 import 'package:food_eye_fyp/utils/http_utils.dart';
 import 'package:provider/provider.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // NotificationService().initNotification();
+  // tz.initializeTimeZones();
   HttpOverrides.global = MyHttpOverrides();
+  // ByteData data =
+  //     await PlatformAssetBundle().load('assets/ca/lets-encrypt-r3.pem');
+  // SecurityContext.defaultContext
+  //     .setTrustedCertificatesBytes(data.buffer.asUint8List());
+
   runApp(
     const MyApp(),
   );
