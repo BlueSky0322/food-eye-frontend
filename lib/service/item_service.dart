82cc849ffcf3b181410f49de128c3283b9bad0e5
BlueSky0322 <ryanng8628@gmail.com>
2023-07-10 14:17:05 +0800

    feat: addItem and updateItem HTTPs calls

--- lib/service/item_service.dart
+++ lib/service/item_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:developer';
 
+import 'package:food_eye_fyp/data/model/item.dart';
 import 'package:http/http.dart';
 
 import '../data/model/item_response.dart';
@@ -18,19 +20,64 @@ class ItemService {
         List<ItemResponseObject> items = responseData
             .map((data) => ItemResponseObject.fromJson(data))
             .toList();
-        print(items);
+        //print(items);
         return items;
       } else {
-        print('API call failed with status code: ${response.statusCode}');
+        log('API call failed with status code: ${response.statusCode}');
         return [];
       }
     } catch (error) {
-      print('Error getting items: $error');
+      log('Error getting items: $error');
       return [];
     }
   }
 
-  Future<ItemResponseObject?> getItemByID(int itemID) async {
+  Future<List<ItemResponseObject>> getItemsExpiringWithin(int days) async {
+    try {
+      final response = await client.get(
+        Uri.parse('$usbDebugURL/api/FoodEyeItems/GetItemsExpiringWithin/$days'),
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> responseData = json.decode(response.body);
+        List<ItemResponseObject> items = responseData
+            .map((data) => ItemResponseObject.fromJson(data))
+            .toList();
+        return items;
+      } else {
+        log('API call failed with status code: ${response.statusCode}');
+        return [];
+      }
+    } catch (error) {
+      log('Error getting items: $error');
+      return [];
+    }
+  }
+
+  Future<List<ItemResponseObject>> getFreshItems() async {
+    try {
+      final response = await client.get(
+        Uri.parse('$usbDebugURL/api/FoodEyeItems/GetFreshItems'),
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> responseData = json.decode(response.body);
+        List<ItemResponseObject> items = responseData
+            .map((data) => ItemResponseObject.fromJson(data))
+            .toList();
+        return items;
+      } else {
+        log('API call failed with status code: ${response.statusCode}');
+        return [];
+      }
+    } catch (error) {
+      log('Error getting items: $error');
+      return [];
+    }
+  }
+
+  Future<ItemResponseObject> getItemByID(int itemID) async {
+    print("Entered getitembyid");
     try {
       final response = await client.get(
         Uri.parse("$usbDebugURL/api/FoodEyeItems/GetItemByID/$itemID"),
@@ -38,18 +85,18 @@ class ItemService {
 
       if (response.statusCode == 200) {
         final responseData = json.decode(response.body);
-        ItemResponseObject item =
-            responseData.map((data) => ItemResponseObject.fromJson(data));
-        print(item);
+        ItemResponseObject item = ItemResponseObject.fromJson(responseData);
+        // log(item);
         return item;
       } else {
-        print('API call failed with status code: ${response.statusCode}');
-        return null;
+        log('API call failed with status code: ${response.statusCode}');
+        // return null;
       }
     } catch (error) {
-      print('Error getting items: $error');
-      return null;
+      log('Error getting items: $error');
+      // return null;
     }
+    return ItemResponseObject();
   }
 
   Future<void> deleteItem(int itemID) async {
@@ -58,12 +105,66 @@ class ItemService {
     try {
       if (response.statusCode == 204) {
         // Deletion successful
-        print('Item deleted successfully');
+        log('Item deleted successfully');
       } else {
-        print('API call failed with status code: ${response.statusCode}');
+        log('API call failed with status code: ${response.statusCode}');
       }
     } catch (error) {
-      print('Error deleting item. Error message: $error');
+      log('Error deleting item. Error message: $error');
+    }
+  }
+
+  Future<bool> updateItem(int id, Item updatedItem) async {
+    final url = Uri.parse('$usbDebugURL/api/FoodEyeItems/UpdateFEItem/$id');
+
+    try {
+      // Convert the updatedItem object to JSON
+      final jsonData = json.encode(updatedItem.toJson());
+
+      // Make the PUT request
+      final response = await client.put(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonData,
+      );
+
+      if (response.statusCode == 204) {
+        log('Item updated successfully!');
+        return true;
+      } else {
+        log('API call failed with status code: ${response.statusCode}');
+        return false;
+      }
+    } catch (error) {
+      log('Error updating item: $error');
+      return false;
+    }
+  }
+
+  Future<bool> addItem(Item newItem) async {
+    final url = Uri.parse('$usbDebugURL/api/FoodEyeItems/AddFEItem');
+
+    try {
+      // Convert the updatedItem object to JSON
+      final jsonData = json.encode(newItem.toJson());
+
+      // Make the PUT request
+      final response = await client.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonData,
+      );
+
+      if (response.statusCode == 201) {
+        log('Item updated successfully!');
+        return true;
+      } else {
+        log('API call failed with status code: ${response.statusCode}');
+        return false;
+      }
+    } catch (error) {
+      log('Error updating item: $error');
+      return false;
     }
   }
 }
