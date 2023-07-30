608c10202e8a1cd7acf22a0b32491c9a0d50a288
BlueSky0322 <ryanng8628@gmail.com>
2023-07-31 02:39:39 +0800

    feat: added input validation with form

--- lib/ui/home_page/components/settings_sidebar/notification_settings.dart
+++ lib/ui/home_page/components/settings_sidebar/notification_settings.dart
@@ -6,7 +6,27 @@ import '../../../../service/shared_preferences_service.dart';
 import '../../../../utils/constants.dart';
 
 class NotificationSettings extends StatelessWidget {
-  const NotificationSettings({super.key});
+  final _formKey = GlobalKey<FormState>();
+  final _textFieldController = TextEditingController();
+
+  NotificationSettings({super.key});
+
+  bool _isValidDayValue(String value) {
+    const pattern = r'^[1-9][0-9]*$';
+    final regExp = RegExp(pattern);
+    return regExp.hasMatch(value);
+  }
+
+  Future<void> _saveNotificationSettings(
+      BuildContext context, int newDaysBeforeToNotify) async {
+    await SharedPrefsService.saveDaysBeforeToNotifyToPrefs(
+        newDaysBeforeToNotify);
+    await BackgroundService.restartDateCheckerBackgroundService();
+
+    Future.microtask(
+      () => Navigator.pop(context),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,44 +43,54 @@ class NotificationSettings extends StatelessWidget {
                 color: primaryBG,
               ),
             ),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      "Current Settings: ",
-                      style: TextStyle(
-                        fontFamily: 'Outfit',
-                        color: Colors.teal.shade800,
+            content: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Current Settings: ",
+                        style: TextStyle(
+                          fontFamily: 'Outfit',
+                          color: Colors.teal.shade800,
+                        ),
                       ),
-                    ),
-                    Text(
-                      "$newDaysBeforeToNotify day(s)",
-                      style: const TextStyle(
-                        fontFamily: 'Outfit',
-                        color: primaryBG,
-                        fontWeight: FontWeight.bold,
+                      Text(
+                        "$newDaysBeforeToNotify day(s)",
+                        style: const TextStyle(
+                          fontFamily: 'Outfit',
+                          color: primaryBG,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextField(
-                  onChanged: (value) {
-                    newDaysBeforeToNotify = int.parse(value);
-                  },
-                  keyboardType: TextInputType.number,
-                  decoration: AddItemInputDecoration.addItemInputDecoration(
-                    labelText: "Notify me before",
-                    hintText: "Days before expiration",
-                    prefixIcon: Icons.notifications_active_rounded,
-                    suffixIcon: null,
+                    ],
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: _textFieldController,
+                    keyboardType: TextInputType.number,
+                    decoration: AddItemInputDecoration.addItemInputDecoration(
+                      labelText: "Notify me before",
+                      hintText: "Days before expiration",
+                      prefixIcon: Icons.notifications_active_rounded,
+                      suffixIcon: null,
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Field cannot be blank';
+                      }
+                      if (!_isValidDayValue(value)) {
+                        return 'Please enter a valid number';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
             ),
             actions: [
               TextButton(
@@ -82,13 +112,11 @@ class NotificationSettings extends StatelessWidget {
                   ),
                 ),
                 onPressed: () async {
-                  await SharedPrefsService.saveDaysBeforeToNotifyToPrefs(
-                      newDaysBeforeToNotify);
-                  await BackgroundService.restartDateCheckerBackgroundService();
-
-                  Future.microtask(
-                    () => Navigator.pop(context),
-                  );
+                  if (_formKey.currentState!.validate()) {
+                    final newDaysBeforeToNotify =
+                        int.parse(_textFieldController.text.trim());
+                    _saveNotificationSettings(context, newDaysBeforeToNotify);
+                  }
                 },
               ),
               TextButton(
