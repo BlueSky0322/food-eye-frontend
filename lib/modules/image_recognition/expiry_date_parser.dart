ebe1f057e080fcf4086e3782ebdeb3afcd5b465a
BlueSky0322 <ryanng8628@gmail.com>
2023-07-31 02:38:42 +0800

    feat: more date format considerations

--- lib/modules/image_recognition/expiry_date_parser.dart
+++ lib/modules/image_recognition/expiry_date_parser.dart
@@ -3,17 +3,25 @@ import 'package:intl/intl.dart';
 
 String parseExpiryDate(String text) {
   final List<String> formats = [
-    'yyyy MM dd', // e.g., "2025 12 12"
-    'dd.MM.yyyy', // e.g., "11.11.2024"
+    'MM dd yyyy', // e.g., "12 24 2025"
+    'dd MM yyyy', // e.g., "24 12 2025"
+    'yyyy MM dd', // e.g., "2025 12 24"
+    'dd.MM.yyyy', // e.g., "17.11.2024"
+    'MM.dd.yyyy', // e.g., "11.17.2024"
+    'yyyy.MM.dd', // e.g., "2024.11.17"
     'dd/MM/yyyy', // e.g., "22/09/2025"
-    'dd.MM.yy', // e.g., "14.11.23"
+    'MM/dd/yyyy', // e.g., "09/22/2025"
+    'yyyy/MM/dd', // e.g., "2025/09/22"
+    'MM-dd-yyyy', // e.g., "07-23-2023"
+    'dd-MM-yyyy', // e.g., "23-07-2023"
     'yyyy-MM-dd', // e.g., "2023-07-03"
+    'dd.MM.yy', // e.g., "14.11.23"
     'dd/MM/yy', // e.g., "01/01/25"
-    'MM/dd/yyyy', // e.g., "12/31/2022"
     'd MMM yyyy', // e.g., "4 Jun 2023"
     'd MMMM yyyy', // e.g., "4 June 2023"
     'MMMM d yyyy', // e.g., "June 4 2023"
   ];
+
   DateTime? expiryDate;
   for (final format in formats) {
     try {
